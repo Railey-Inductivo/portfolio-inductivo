1ecd4a96e2b9caa985b669f426d161ff55ab0a07
Railey Inductivo <80341106+RaileyAnthony@users.noreply.github.com>
2025-04-07 22:32:50 +0400

    commit

--- src/main.js
+++ src/main.js
@@ -9,3 +9,28 @@ function toggleMenu() {
     menuList.style.maxHeight = "0px";
   }
 }
+
+var swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
